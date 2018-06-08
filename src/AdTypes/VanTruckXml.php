29c9abed6e304ebf302231a06c62d074bd5006a8
Eirik S. Morland <eirik@morland.no>
2018-06-08 12:33:00 +0200

    Fill up what we need in van truck xml

--- src/AdTypes/VanTruckXml.php
+++ src/AdTypes/VanTruckXml.php
@@ -18,13 +18,43 @@ class VanTruckXml extends AdType
 
   public function __construct($partner_id, $provider) {
     parent::__construct($partner_id, $provider);
+    $this->setSegment('');
+    $this->createModelProperty('VAN_MODEL');
+    $this->adBody->appendChild($this->modelOuterBody);
+    $this->YEAR_MODEL = '';
+    $this->MILEAGE = '';
+    $this->createMotorPriceElements();
+    $this->createEngineElements();
+    $this->WEIGHT = '';
+    $this->REGISTRATION_FIRST = '';
+    $this->NO_OF_SEATS = '';
+    $this->VAN_EQUIPMENT = '';
+    $this->LENGTH_CM = '';
+    $this->TRANSMISSION_SPECIFICATION = '';
+    $this->CABIN_TYPE = '';
+    $this->PERMITTED_LOAD = '';
+    $this->REGNO = '';
+    $this->EU_APPROVED = '';
+    $this->SUSPENSION_FRONT = '';
+    $this->SUSPENSION_REAR = '';
+    $this->TYPE_OF_AXLE = '';
+    $this->WHEELBASE = '';
+    $this->EXTRA_BRAKE = '';
+    $this->CHASSIS_NO = '';
+    $this->BOX_TYPE = '';
+    $this->BOX_LENGTH = '';
+    $this->INTERIOR_MEASUREMENTS = '';
+    $this->TAILLIFT = '';
+    $this->DESCRIPTION = '';
+    $this->createMoreInfoElements();
+    $this->initializeContact();
+    $this->CAR_LOCATION = '';
+    $this->PALLET = '';
   }
 
   public function setSegment($segment)
   {
     $this->VAN_SEGMENT = $segment;
-    $this->createModelProperty('VAN_MODEL');
-    $this->adBody->appendChild($this->modelOuterBody);
   }
 
 }
