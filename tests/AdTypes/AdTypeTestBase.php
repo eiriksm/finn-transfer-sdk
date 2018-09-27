53d4b04a799377bd3ea5cdb35f94c85ddd16c69b
Eirik S. Morland <eirik@morland.no>
2018-09-27 14:00:05 +0200

    Make sure test folder is created

---
+++ tests/AdTypes/AdTypeTestBase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace eiriksm\FinnTransfer\Tests\AdTypes;
+
+use eiriksm\FinnTransfer\AdType;
+use PHPUnit\Framework\TestCase;
+
+class AdTypeTestBase extends TestCase
+{
+
+    protected $className;
+
+    /**
+     * Test that we can instantiate without error.
+     */
+    public function testConstructor()
+    {
+        $partner_id = 'test';
+        $provider = 'test';
+        $class = new $this->className($partner_id, $provider);
+        $this->assertTrue($class instanceof AdType);
+    }
+}
