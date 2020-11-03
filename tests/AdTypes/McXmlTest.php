cb5d835cc53fcbfa0c095b6d8e63b0913fc306b2
Eirik Stanghelle Morland <eirik@morland.no>
2020-11-03 17:01:23 +0200

    Add some new types (#5)
    * Add some new types
    
    * Code style
    
    * Stop being strict about covers annotation
    
    * Add tests for the new classes

---
+++ tests/AdTypes/McXmlTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace eiriksm\FinnTransfer\Tests\AdTypes;
+
+use eiriksm\FinnTransfer\AdTypes\McXml;
+
+class McXmlTest extends AdTypeTestBase
+{
+
+    protected $className = McXml::class;
+}
---
+++ tests/AdTypes/TorgetXmlTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace eiriksm\FinnTransfer\Tests\AdTypes;
+
+use eiriksm\FinnTransfer\AdTypes\TorgetXml;
+
+class TorgetXmlTest extends AdTypeTestBase
+{
+
+    protected $className = TorgetXml::class;
+}
