869d2692ef3f741d2c2284b73aba9a6a715ec9ae
Eirik S. Morland <eirik@morland.no>
2020-11-04 13:20:18 +0100

    Add a test

---
+++ tests/AdTypes/CaravanXmlTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace eiriksm\FinnTransfer\Tests\AdTypes;
+
+use eiriksm\FinnTransfer\AdTypes\CaravanXml;
+
+class CaravanXmlTest extends AdTypeTestBase
+{
+
+    protected $className = CaravanXml::class;
+}
