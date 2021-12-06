4ee6892042df189c813c780dea22a47ab5151863
Eirik Stanghelle Morland <eirik@morland.no>
2021-12-06 17:39:45 +0200

    Add basic test coverage for new classes (#11)
    * Add basic test coverage for new classes
    
    * Code style and typo in classname

---
+++ tests/AdTypes/BoatMotorXmlTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace eiriksm\FinnTransfer\Tests\AdTypes;
+
+use eiriksm\FinnTransfer\AdTypes\BoatMotorXml;
+
+class BoatMotorXmlTest extends AdTypeTestBase
+{
+
+    protected $className = BoatMotorXml::class;
+}
---
+++ tests/AdTypes/MotorMiscXmlTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace eiriksm\FinnTransfer\Tests\AdTypes;
+
+use eiriksm\FinnTransfer\AdTypes\MotorMiscXml;
+
+class MotorMiscXmlTest extends AdTypeTestBase
+{
+
+    protected $className = MotorMiscXml::class;
+}
