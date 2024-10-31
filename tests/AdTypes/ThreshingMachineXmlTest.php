4ae7b6771e7d7e2bdee76d7ecd8bf22a6721b892
Eirik S. Morland <eirik@morland.no>
2024-10-31 10:29:10 +0100

    Add tests

--- tests/AdTypes/ThreshingMachineXmlTest.php
+++ tests/AdTypes/ThreshingMachineXmlTest.php
@@ -2,10 +2,22 @@
 
 namespace eiriksm\FinnTransfer\Tests\AdTypes;
 
+use eiriksm\FinnTransfer\AdType;
 use eiriksm\FinnTransfer\AdTypes\ThreshingMachineXml;
 
 class ThreshingMachineXmlTest extends AdTypeTestBase
 {
 
     protected $className = ThreshingMachineXml::class;
+
+    public function testAddEquipment()
+    {
+        $partner_id = 'test';
+        $provider = 'test';
+        $class = new ThreshingMachineXml($partner_id, $provider);
+        $class->addEquipment(['test']);
+        $this->assertTrue($class instanceof AdType);
+        $xml = $class->getXml();
+        $this->assertNotFalse($xml);
+    }
 }
